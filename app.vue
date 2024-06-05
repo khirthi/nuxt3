a79d2447c3b5ed677499676187313d73d6d3f6b0
khirthi <khirthikumar1215@gmail.com>
2024-06-05 20:12:43 +0530

    added nuxt meta

--- app.vue
+++ app.vue
@@ -1,11 +1,25 @@
 <script setup lang="ts">
-const version = 78
-const amazon = 800
+const title = `Carnival`
+const description = ref('Carnival Description')
+const name = `Carnival Name`
+
+// This will be reactive when you change title/description above
+useHead({
+  title,
+  meta: [{
+    name: name,
+    content: description
+  }]
+})
 </script>
 
 <template>
   <div class="hello">
-    Hello World!
+    <Head>
+      <Title>{{title}}</Title>
+      <H2>Hello world</H2>
+      <Meta name = name :content="description" />
+    </Head>
   </div>
 </template>
 
