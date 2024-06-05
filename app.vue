07352b88d9ec1d6de62d0870be78b1ce7c4abf51
khirthi <khirthikumar1215@gmail.com>
2024-06-05 19:08:03 +0530

    initial commit

---
+++ app.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const version = 78
+const amazon = 800
+</script>
+
+<template>
+  <div class="hello">
+    Hello World!
+  </div>
+</template>
+
+<style scoped>
+.hello {
+  font-family: Roboto, Helvetica, sans-serif;
+  font-size: 3rem;
+  padding: 2rem;
+}
+</style>
